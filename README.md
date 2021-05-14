de389324a7f353ae98f95f14e10092f44760b890
Marco Martins <marcodanielmartins@gmail.com>
2021-05-14 15:13:24 +0200

    Cleanup types

---
+++ README.md
@@ -0,0 +1,5 @@
+# environment decoder
+
+> A decoder for your `process.env` into a typed object
+
+`environment-decoder` allows you to define an "interface" for the environment variables you need for your application.
--- src/debug.ts
+++ src/debug.ts
@@ -1,15 +1,19 @@
-import {environmentDecoder, string, DecodeType, literal} from "./"
+import {environmentDecoder, asString, DecodeType, asNumber, asBoolean} from "./"
 
-const env = environmentDecoder(process.env, {
-    USER: string,
-    WHAT: literal('123'),
+const env = environmentDecoder({
+    USER: asString,
+    WHAT: asString,
+    WHY: asNumber,
+    FAIL: asBoolean
 })
 
 type Stuff = DecodeType<typeof env>
 
 const a: Stuff = {
     USER: '',
-    WHAT: '123'
+    WHAT: '123',
+    WHY: 123,
+    FAIL: false
 }
 
 console.log(a, env)
